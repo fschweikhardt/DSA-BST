fac47c09e6bc997e06a868b047bce825106edeaa
fschweikhardt <schweikhardt.web@gmail.com>
2021-04-11 17:32:39 -0400

    Update BST-drills.js

--- BST-drills.js
+++ BST-drills.js
@@ -1,11 +1,75 @@
 const BinarySearchTree = require('./BST')
 
-function drawBST() {
-    const BST = new BinarySearchTree()
+const BST = new BinarySearchTree()
+function drawBST() { 
     let data = [3,1,4,6,9,2,5,7]
-    data.forEach(x => BST.insert(x))
-
+    //let data = [5,1,4,6,9,2,7]
+    data.forEach(x => BST.insert(x, `value = ${x}`))
     return BST
 }
 
-console.log(drawBST())
+drawBST()
+//console.log(drawBST())
+
+function heightOfBST(bst) {
+    let leftHeight = 0;
+    let rightHeight = 0;
+    if (!bst) {
+      return 0;
+    } else {
+      leftHeight = heightOfBST(bst.left);
+      rightHeight = heightOfBST(bst.right);
+      if (leftHeight > rightHeight) {
+        return leftHeight + 1;
+      } else {
+        return rightHeight + 1;
+      }
+    }
+  }
+
+//console.log(heightOfBST(drawBST()))
+
+function bst_height1(tree) {
+    return (
+      Math.max(
+        tree.left && bst_height1(tree.left),
+        tree.right && bst_height1(tree.right)
+      ) + 1
+    );
+}
+
+//console.log(bst_height1(drawBST()))
+
+function bst_height2(tree) {
+    if (tree.left && tree.right)
+      return Math.max(bst_height2(tree.left), bst_height2(tree.right)) + 1;
+    if (tree.left) return bst_height2(tree.left) + 1;
+    if (tree.right) return bst_height2(tree.right) + 1;
+    return 1;
+}
+
+//console.log(bst_height2(drawBST()))
+
+
+function isItBSTree(tree) {
+    if (!tree) return false
+  
+    if (tree.right) {
+      if (tree.right.key > tree.key) {
+        isItBSTree(tree.right)
+      } else {
+        return false;
+      }
+    }
+  
+    if (tree.left) {
+      if (tree.left.key < tree.key) {
+        isItBSTree(tree.left)
+      } else {
+        return false
+      }
+    }
+    return true
+}
+
+console.log(isItBSTree(drawBST()))
